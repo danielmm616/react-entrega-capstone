7acd641a33e39819b741d0c6e395c45ca44fc375
Márcio Alves <alvesmarcio@hotmail.com>
2022-01-25 11:15:12 -0300

    fix: conecta os providers com a pagina de login

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -14,6 +14,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useHistory } from "react-router-dom";
 import * as yup from "yup";
+import { useAuth } from "../../Providers/AuthContext";
 
 interface UserData {
   email: string;
@@ -22,6 +23,7 @@ interface UserData {
 
 const LoginPage = () => {
   const history = useHistory();
+  const { logIn } = useAuth();
 
   const schema = yup.object().shape({
     email: yup.string().email("Invalid email.").required("Email required."),
@@ -37,7 +39,7 @@ const LoginPage = () => {
   });
 
   const onSubmitFunction = (user: UserData) => {
-    // logIn(user);
+    logIn(user);
     history.push("/shop");
   };
 
