10ab87b784d7cddd2647b4a0b440953b616c3d6f
Márcio Alves <alvesmarcio@hotmail.com>
2022-01-25 11:20:03 -0300

    fix: conecta Providers com a RegisterPage

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,11 +1,15 @@
 import { Switch, Route } from "react-router-dom";
 
+import RegisterPage from "../Pages/RegisterPage";
+
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/"></Route>
       <Route path="/login"></Route>
-      <Route path="/register"></Route>
+      <Route path="/register">
+        <RegisterPage />
+      </Route>
       <Route path="/shop"></Route>
     </Switch>
   );
