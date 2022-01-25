0d5f9cee37f37900c3cdb1a19f05fddbd178d49c
Márcio Alves <alvesmarcio@hotmail.com>
2022-01-25 15:34:41 -0300

    fix: Login adicionado as rotas

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,13 +1,14 @@
 import { Switch, Route } from "react-router-dom";
 import LoginPage from "../Pages/Login";
-
 import RegisterPage from "../Pages/RegisterPage";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/"></Route>
-      <Route path="/login"></Route>
+      <Route path="/login">
+        <LoginPage />
+      </Route>
       <Route path="/register">
         <RegisterPage />
       </Route>
