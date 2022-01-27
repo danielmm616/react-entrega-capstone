f739a9a4c8b8bcb64747ff61b7da6046b06290c4
Daniel <ddan.mm.16@gmail.com>
2022-01-27 10:10:08 -0300

    fix: removendo input do componente Header e adicionando animação e funcionalidade aos icones

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Routes from "./Routes";
 import "./App.css";
+import HeaderC from "./components/HeaderC";
 
 function App() {
   return (
     <div className="App">
+      <HeaderC />
       <Routes />
     </div>
   );
