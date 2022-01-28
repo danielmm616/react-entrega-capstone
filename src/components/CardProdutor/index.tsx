83f8d6f006fec34d4882c020606a4c561af9b88d
yasmin <yasminmartinsdebrito@gmail.com>
2022-01-28 11:40:55 -0300

    styles:arrumando o estilo de CardProdutor

--- src/components/CardProdutor/index.tsx
+++ src/components/CardProdutor/index.tsx
@@ -22,20 +22,20 @@ const CardProdutor = ({name,state,city}:User, {ovos,mel,verduras,legumes,frutas}
                  <div  className="flip-card-inner">
                     <div className="flip-card-front">
                         <h1>{name = "Yasmin"}</h1>
-                        <img src='https://creazilla-store.fra1.digitaloceanspaces.com/emojis/52524/man-farmer-emoji-clipart-xl.png' alt=''/>
+                        <img className='imgProdutor'src='https://creazilla-store.fra1.digitaloceanspaces.com/emojis/52524/man-farmer-emoji-clipart-xl.png' alt=''/>
                         <div className='frente'>
-                          <p>{state = 'Sp'}</p>
-                          <p>{city = 'Mococa'}</p>
+                          <p className='estadoCidade'>{state = 'Sp'}</p>
+                          <p className='estadoCidade'>{city = 'Mococa'}</p>
                         </div>
                    </div> 
                     <div className="flip-card-back">
                            <ul>
-                               <li>{ovos = 'Ovos'}</li>
-                               <li>{mel = 'Mel'}</li>
-                               <li>{verduras = 'Verduras'}</li>
-                               <li>{legumes = 'Legumes'}</li>
-                               <li>{frutas = 'Maçã'}</li>
-                               <li>{frutas = 'Banana'}</li>
+                               <li className='li-flip'>{ovos = 'Ovos'}</li>
+                               <li className='li-flip'>{mel = 'Mel'}</li>
+                               <li className='li-flip'>{verduras = 'Verduras'}</li>
+                               <li className='li-flip'>{legumes = 'Legumes'}</li>
+                               <li className='li-flip'>{frutas = 'Maçã'}</li>
+                               <li className='li-flip'>{frutas = 'Banana'}</li>
                           </ul>
                     </div>
                 </div> 
