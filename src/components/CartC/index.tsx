3c71115d0f3d56e2506e4c7d4a7bc503e4f56e75
Matteo Basso <matteo.brazil@hotmail.it>
2022-01-31 15:39:06 -0300

    Progresso com carrinhos e cards carrinhos

--- src/components/CartC/index.tsx
+++ src/components/CartC/index.tsx
@@ -1,20 +1,44 @@
 import { Box, Text, Flex } from "@chakra-ui/react";
 import ButtonC from "../ButtonC";
 import { useHistory } from "react-router-dom";
+import CardCartC from "../CardCartC";
+import { ReactNode } from "react";
 
-const CartC = () => {
+interface CartCProps {
+  children: ReactNode;
+}
 
-    const history = useHistory()
+const CartC = ({ children }: CartCProps) => {
+  
+  const history = useHistory();
+
   return (
     <Box color="white" w="300px" h="450px" bg="brown.300" borderRadius="10px">
       <Text p="10px">Compra atual</Text>
-      <Box w="300px" h="300px" bg="gray.100"></Box>
-      <Flex mb="20px">
+      <Box w="300px" h="300px" bg="gray.100" overflowY="auto"
+      sx={{
+        '&::-webkit-scrollbar': {
+          width: '16px',
+          borderRadius: '8px',
+          backgroundColor: `gray.100`,
+        },
+        '&::-webkit-scrollbar-thumb': {
+          backgroundColor: `green.200`,
+          borderRadius: '8px'
+        },
+      }}>
+        {children}
+      </Box>
+      <Flex mb="20px" justify="space-between" p="0 10px">
         <Text>Total</Text>
         <Text>R$</Text>
       </Flex>
 
-      <ButtonC onClick={()=> history.push('/cart')} text="Ir para o carrinho" bg="green.200" />
+      <ButtonC
+        onClick={() => history.push("/cart")}
+        text="Ir para o carrinho"
+        bg="green.200"
+      />
     </Box>
   );
 };
