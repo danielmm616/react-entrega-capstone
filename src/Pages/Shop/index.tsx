e39e4f72ba8043b6ca58cf4f30a7be1bbe788a0b
yasmin <yasminmartinsdebrito@gmail.com>
2022-01-31 13:42:10 -0300

    feature:Finalizei o Form para poder adicionar os produtos, e fiz uma estilização simples

--- src/Pages/Shop/index.tsx
+++ src/Pages/Shop/index.tsx
@@ -1,9 +1,12 @@
+import FormProducts from "../../components/FormProducts"
 import ProductAddToCart from "../../components/Products"
 
 const Shop = () => {
     return(
         <>
          <strong>Produtos</strong>
+         <br/>
+        <FormProducts/>
         <ProductAddToCart/>
         </>
     )
