4d7a670d4634e42993d64de7c00fecc0f74e6c25
yasmin <yasminmartinsdebrito@gmail.com>
2022-02-01 13:23:10 -0300

    feature: ajustando os cards

--- src/Pages/Shop/index.tsx
+++ src/Pages/Shop/index.tsx
@@ -1,9 +1,7 @@
 import { Flex } from "@chakra-ui/react";
 import CartC from "../../components/CartC";
-import FormProducts from "../../components/FormProducts";
 import HeaderC from "../../components/HeaderC";
 import ProductAddToCart from "../../components/Products";
-import ProdutsEdits from "../../components/ProdutsEdits";
 import { Redirect } from "react-router-dom";
 
 const Shop = () => {
@@ -16,12 +14,10 @@ const Shop = () => {
     <>
       <HeaderC />
       <br />
-      <FormProducts />
       <Flex m={[8, 16]}>
         <ProductAddToCart />
         <CartC>{}</CartC>
       </Flex>
-      <ProdutsEdits />
     </>
   );
 };
