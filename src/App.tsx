f3b7d9ec2d9ac0ec1ccb09f14c22842b4264c3e2
yasmin <yasminmartinsdebrito@gmail.com>
2022-02-01 15:06:03 -0300

    feat:atualizar developer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import Routes from "./Routes";
 import "./App.css";
-import Shop from "./Pages/Shop";
 
 function App() {
   return (
     <div className="App">
-      <Shop />
+      
       <Routes />
     </div>
   );
