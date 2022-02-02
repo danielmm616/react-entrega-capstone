2830b253c8d6ae702da251b5ba224a5973e8dd2b
Emerson Pereira <ebergp@gmail.com>
2022-02-02 18:23:23 -0300

    fix: commit antes de começar alterações

--- src/components/CartC/index.tsx
+++ src/components/CartC/index.tsx
@@ -3,8 +3,7 @@ import ButtonC from "../ButtonC";
 import { useHistory } from "react-router-dom";
 
 const CartC = () => {
-
-    const history = useHistory()
+  const history = useHistory();
   return (
     <Box color="white" w="300px" h="450px" bg="brown.300" borderRadius="10px">
       <Text p="10px">Compra atual</Text>
@@ -14,7 +13,11 @@ const CartC = () => {
         <Text>R$</Text>
       </Flex>
 
-      <ButtonC onClick={()=> history.push('/cart')} text="Ir para o carrinho" bg="green.200" />
+      <ButtonC
+        onClick={() => history.push("/cart")}
+        text="Ir para o carrinho"
+        bg="green.200"
+      />
     </Box>
   );
 };
